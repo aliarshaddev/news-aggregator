63b2646577df10225ce69c32c2aa33fb1fa907dc
devaliarshad <aliarshad1566@gmail.com>
2024-11-11 04:51:35 +0500

    docker testcases and other fixes

--- database/migrations/2024_11_08_190730_create_user_preferences_table.php
+++ database/migrations/2024_11_08_190730_create_user_preferences_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('user_preferences', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('sources')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
             $table->json('preferred_sources')->nullable();
             $table->json('preferred_categories')->nullable();
             $table->json('preferred_authors')->nullable();
